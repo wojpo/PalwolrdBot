231c1496e3716715f56e85574c1a87f0379e1d30
Wojciech Piech <ilikeahottodogu@gmail.com>
2024-01-30 22:26:36 +0100

    Paldeckentry command added

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-# This example requires the 'message_content' privileged intents
-
 import os
 import discord
+from discord import app_commands
 from discord.ext import commands
-
+import requests
+from Token import token
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -14,13 +14,23 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 async def on_ready():
     print(f"Logged in as {bot.user}")
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
-
-@bot.command()
-async def hello(ctx):
-    await ctx.send("Choo choo! 🚅")
+    synced = await bot.tree.sync()
 
 
-bot.run(os.environ["DISCORD_TOKEN"])
+@bot.tree.command(name='paldeck-entry')
+async def paldeckinfo(ctx: discord.Interaction, palname: str):
+    endpoint_url = f'http://127.0.0.1:8000/Pal={palname}'
+    response = requests.get(endpoint_url)
+    if response.status_code == 200:
+        json_data = response.json()
+        paldeck = json_data.get('Paldeck')
+        paldeckEntry = paldeck.get('PaldeckEntry')
+        print(f"Parameter Value: {paldeckEntry}")
+        embed = discord.Embed(title=f"{palname}", description=f"{paldeckEntry}")
+        await ctx.response.send_message(embed=embed)
+    else:
+        embed = discord.Embed(title="ERROR", description="Wrong pal name given")
+        await ctx.response.send_message(embed=embed)
+
+
+bot.run(f'{token}')
