f00f43f31bf192775f9338eef82f47978790fc1e
Wojciech Piech <ilikeahottodogu@gmail.com>
2024-02-01 22:04:55 +0100

    Update info.py

--- routes/info.py
+++ routes/info.py
@@ -3,10 +3,13 @@ from discord import Color
 
 
 async def info(ctx: discord.Interaction):
-    embed = discord.Embed(title='HELP', color=Color.green(), description='Bot made by ``wojpo`` which returns info like '
-                                                                         '`paldeck`, '
-                                                                         '`elements`, `drops`, `work_suitability`, '
-                                                                         '`partner_skill`, `skills` about pals')
+    embed = discord.Embed(title='HELP', color=Color.green(), description='Bot made by ``wojpo`` which returns data '
+                                                                         'about pals'
+                                                                         'from '
+                                                                         '`https://palworld.fandom.com/wiki'
+                                                                         '/Palworld_Wiki`'
+                          )
+    embed.add_field(name="Commands", value='`paldeck`, `elements`, `drops`, `work_suitability`, `partner_skill`, '
+                                           '`skills`')
     embed.set_footer(text='https://discord.gg/wQaF6Peazu')
     await ctx.response.send_message(embed=embed)
-    
